233802ce5bee5bb34e788bd9d0e688987ba7d464
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-12-14 23:40:19 +0900

    초기 테스팅

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>RN APP 만들기 테스트</Text>
       <StatusBar style="auto" />
     </View>
   );
